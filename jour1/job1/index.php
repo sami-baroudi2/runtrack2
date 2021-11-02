d161b97ce98dfeac18eb2d47ec7dcf8a55e3dff8
sami <sami.baroudi@laplateforme.io>
2021-11-02 13:55:54 +0100

    job1

--- jour1/job1/index.php
+++ jour1/job1/index.php
@@ -7,4 +7,17 @@ $str3 = " !";
 
 echo $str2 . $str . $str3;
 
+echo "</br>";
+$val = 6;
+echo $val;
+echo "</br>";
+$val = 4;
+echo $val;
+
+echo "</br>";
+$myBool = true ;
+echo $myBool;
+$myBool = false;
+echo $myBool;
+
 ?>
