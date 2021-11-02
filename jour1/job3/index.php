4dc7f4ca0addc0181755dbf22cf8467801c70f4a
sami <sami.baroudi@laplateforme.io>
2021-11-02 23:07:34 +0100

    job3

--- jour1/job3/index.php
+++ jour1/job3/index.php
@@ -1,1 +1,43 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Job3</title>
+</head>
+<body>
+    <table>
+    <tr>
+        <td>Type</td>
+        <td>Nom</td>
+        <td>Valeur</td>
+    </tr>
+    <tr>
+        <td>Variable primitive</td>
+        <td>Booléen</td>
+        <td><?php $Bool = true;
+        echo $Bool;?></td>
+    </tr>
+    <tr>
+        <td>Variable primitive</td>
+        <td>Entier</td>
+        <td><?php $int = 42;
+        echo $int;?></td>
+    </tr>
+    <tr>
+        <td>Variable primitive</td>
+        <td>Chaînes de caractères</td>
+        <td><?php $str ="String";
+        echo $str;?></td>
+    </tr>
+    <tr>
+        <td>Variable primitive</td>
+        <td>Nombre à virgule flottante</td>
+        <td><?php $float = 3.14;
+        echo $float;?><td>
+    </tr>
 
+    </table>
+    
+</body>
+</html>
