a797c20480d0ece6f7a9697006b793a8442fc603
sami <sami.baroudi@laplateforme.io>
2021-11-03 12:01:50 +0100

    job2

--- jour2/job2/index.php
+++ jour2/job2/index.php
@@ -1,1 +1,10 @@
+<?php
+$number=0;
 
+while ($number<=1337): 
+    $number++;
+    if ($number != 26 && $number !=37 && $number != 88 && $number !=1111){
+        echo $number."</br>";
+    }
+endwhile;
+?>
