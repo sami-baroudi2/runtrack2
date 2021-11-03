68e89e511aab750a57548a56c93d1d7f224b9711
sami <sami.baroudi@laplateforme.io>
2021-11-03 15:26:27 +0100

    job3

--- jour2/job3/index.php
+++ jour2/job3/index.php
@@ -2,15 +2,17 @@
 $number=0;
 while ($number<=99):
     $number++;
-    if ($number<=25){
+    if ($number<=20){
         echo "</br>"."<i>".$number."</i>";
     }
     elseif ($number==42) {
         echo "</br>"."LaPlateforme_";
     }
-    
     elseif ($number>=25 && $number<=50){
         echo "</br>"."<u>".$number."</u>";
+    }
+    elseif ($number>=20){
+        echo "</br>".$number;
     }
     elseif ($number>=50) {
         echo "</br>".$number;
