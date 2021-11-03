ab6ed7d7baddeb229dd67693a02d2198b7e5428a
sami <sami.baroudi@laplateforme.io>
2021-11-03 13:51:05 +0100

    job4

--- jour2/job4/index.php
+++ jour2/job4/index.php
@@ -1,1 +1,18 @@
-
+<?php 
+$number=0;
+while ($number<=99): 
+    $number++;
+    if (!($number%3) && !($number%5)) {
+        echo "</br>"."FizzBuzz";
+    }
+    elseif (!($number%3)) {
+        echo "</br>"."Fizz";
+    } 
+    elseif (!($number%5)) {
+        echo "</br>"."Buzz";
+    }
+    else {
+        echo "</br>".$number;
+    }
+endwhile;
+?>
