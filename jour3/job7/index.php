af913b1570893ba4a0e8f4928f997285bce3c720
sami-baroudi2 <91854540+sami-baroudi2@users.noreply.github.com>
2021-11-04 20:39:26 +0100

    job 7 - Final

---
+++ jour3/job7/index.php
@@ -0,0 +1,9 @@
+<?php 
+$c = ['C'];
+$str="Certaines choses changent, et d'autres ne changeront jamais.";
+    for ($index = 1; isset( $str[ $index ]) == true; ++$index) {
+        echo $str[ $index ] ;
+        }
+echo $c[0];
+    
+?>
