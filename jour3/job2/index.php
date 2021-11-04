03b567cb9521c3b49b7d15fc6869112d5f2d307f
sami <sami.baroudi@laplateforme.io>
2021-11-04 16:13:50 +0100

    final job2

--- jour3/job2/index.php
+++ jour3/job2/index.php
@@ -1,1 +1,8 @@
+<?php
+$str="Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+    for ($i = 0;isset($str[$i]);$i+=2) {
+        echo $str[$i]. ' ' ;
+        }
 
+    
+?>
