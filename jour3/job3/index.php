0c396ca527b87eef84ad25547148afff326a6af8
sami <sami.baroudi@laplateforme.io>
2021-11-04 18:12:57 +0100

    final job 3 - ajout des commentaires

--- jour3/job3/index.php
+++ jour3/job3/index.php
@@ -1,1 +1,27 @@
+<?php
+$str = "I'm sorry Dave I'm afraid I can't do that.";
+$Tabvoy = ['a','e','i','o','u','y','A','E','I','O','U','Y']; 
 
+    //
+
+    for ($index = 0; isset( $str[ $index ]) == true; $index++) // Pour chaque caractère de la chaîne "str" 
+    {
+        $CaractereStr = $str[ $index ]; 
+
+        // Recherche si le caractère est dans la table des voyelles 
+        for ( $indexVoyelle = 0; isset( $Tabvoy[ $indexVoyelle ]) == true; $indexVoyelle++) // Pour chaque voyelle du tableau Tabvoy
+        {
+              $Voyelle = $Tabvoy[ $indexVoyelle ];
+              
+            if ( $CaractereStr == $Voyelle ) {
+                // C'est une voyelle
+                echo $CaractereStr. ' ' ;//Affiche le caractère de str qui est une voyelle
+                break ; // Sortie de boucle
+               
+            
+            } 
+        } /// fin Bcl Voyelle
+        
+        
+    } // Fin for
+?>
