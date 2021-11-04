26ee8903f806047c090fdbfc6f29b59ff24daa8c
sami <sami.baroudi@laplateforme.io>
2021-11-04 12:34:25 +0100

    job1 - tableau

--- jour3/job1/index.php
+++ jour3/job1/index.php
@@ -1,1 +1,11 @@
+<?php
+$tab = ['nombres'=> ['200', '204', '173', '98' , '171' , '404' , '459']];
 
+echo "</br>".$tab['nombres'][0]. ' '."Paire";
+echo "</br>".$tab['nombres'][1]. ' ' ."Paire";
+echo "</br>".$tab['nombres'][2]. ' ' ."Impaire";
+echo "</br>".$tab['nombres'][3]. ' ' ."Paire";
+echo "</br>".$tab['nombres'][4]. ' ' ."Impaire";
+echo "</br>".$tab['nombres'][5]. ' ' ."Paire";
+echo "</br>".$tab['nombres'][6]. ' ' ."Impaire";
+?>
