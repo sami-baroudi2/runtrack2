9001678bd3bf675801147311e2f52afceddd5a12
sami <sami.baroudi@laplateforme.io>
2021-11-04 14:35:18 +0100

    commit final job1

--- jour3/job1/index.php
+++ jour3/job1/index.php
@@ -1,11 +1,21 @@
 <?php
-$tab = ['nombres'=> ['200', '204', '173', '98' , '171' , '404' , '459']];
+$tab = [200, 204, 173, 98 , 171 , 404 , 459];
 
-echo "</br>".$tab['nombres'][0]. ' '."Paire";
-echo "</br>".$tab['nombres'][1]. ' ' ."Paire";
-echo "</br>".$tab['nombres'][2]. ' ' ."Impaire";
-echo "</br>".$tab['nombres'][3]. ' ' ."Paire";
-echo "</br>".$tab['nombres'][4]. ' ' ."Impaire";
-echo "</br>".$tab['nombres'][5]. ' ' ."Paire";
-echo "</br>".$tab['nombres'][6]. ' ' ."Impaire";
+/*echo "</br>".$tab[0]. ' '."Paire";
+echo "</br>".$tab[1]. ' ' ."Paire";
+echo "</br>".$tab[2]. ' ' ."Impaire";
+echo "</br>".$tab[3]. ' ' ."Paire";
+echo "</br>".$tab[4]. ' ' ."Impaire";
+echo "</br>".$tab[5]. ' ' ."Paire";
+echo "</br>".$tab[6]. ' ' ."Impaire";*/
+
+foreach ($tab  as $value) {
+    if ($value%2 == 0) {
+        echo $value.' '. 'Paire'.'</br>';
+    }
+    else {
+        echo $value.' '.'Impaire'.'</br>';
+    }
+   
+}
 ?>
