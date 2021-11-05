602f8e82834aaafefb09d64c3fe2eb87a87dbd1c
sami <sami.baroudi@laplateforme.io>
2021-11-05 14:42:44 +0100

    commit job 1 - Final

--- jour4/job1/index.php
+++ jour4/job1/index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job1</title>
+</head>
+<body>
+    <form action="index.php" method="GET">
+        <p>
+            <label for="pseudo">Pseudo</label> : 
+        <input type="pseudo" name="pseudo" required>
+            </br></br>
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" required>
+        </br></br>
+        <input type="submit" name="Envoyer">
+        
+     </form>
+    
+</body>
+</html>
+<?php
+echo $_GET['pseudo'] . "</br>" . $_GET['password'];
 
+?>
