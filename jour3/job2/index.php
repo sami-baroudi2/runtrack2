db5b515f8362be06709a05d9ba45f858513f3efe
sami <sami.baroudi@laplateforme.io>
2021-11-05 11:41:32 +0100

    final

--- jour3/job2/index.php
+++ jour3/job2/index.php
@@ -1,8 +1,6 @@
 <?php
 $str="Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
-    for ($i = 0;isset($str[$i]);$i+=2) {
+    for ( $i = 0; isset ($str [ $i ] ) ; $i+= 2 ) {
         echo $str[$i]. ' ' ;
         }
-
-    
 ?>
--- jour3/job4/index.php
+++ jour3/job4/index.php
@@ -1,6 +1,5 @@
 <?php
-$str="Dans l'espace, personne ne
-vous entend crier.";
+$str="Dans l'espace, personne ne vous entend crier.";
     for ($Cara=0; isset($str[$Cara]); $Cara++) { 
         echo $Cara;
     }
