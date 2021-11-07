cb728a8e73b91ca9168174b5bef959f3f7d12ab3
sami <sami.baroudi@laplateforme.io>
2021-11-07 23:27:14 +0100

    job 6

--- jour4/job6/index.php
+++ jour4/job6/index.php
@@ -1,11 +1,11 @@
 <?php 
  
-if ($_POST['nombre'] %2 == 1 ) {
-    echo $_POST['nombre'] . ' ' .' Est impaire!';
+if ($_GET['nombre'] %2 == 1 ) {
+    echo $_GET['nombre'] . ' ' .' Est impaire!';
   
 }  
 else {
-    echo $_POST['nombre'] . ' ' . 'Est paire!';
+    echo $_GET['nombre'] . ' ' . 'Est paire!';
 }
 
 ?>
@@ -19,10 +19,11 @@ else {
     <title>job6</title>
 </head>
 <body>
-    <form action ="index.php" method="POST">
+    <form action ="index.php" method="GET">
         <label for="nombre">Nombre</label>
         <input type="text" name="nombre"> </br>
         <input type="submit" name="submit">
+    </form>
     
 </body>
 </html>
