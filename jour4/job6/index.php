0ed737b3d02946c13763f59306e09a2a6ca141ed
sami <sami.baroudi@laplateforme.io>
2021-11-07 22:51:57 +0100

    job 6 final

--- jour4/job6/index.php
+++ jour4/job6/index.php
@@ -1,1 +1,28 @@
+<?php 
+ 
+if ($_POST['nombre'] %2 == 1 ) {
+    echo $_POST['nombre'] . ' ' .' Est impaire!';
+  
+}  
+else {
+    echo $_POST['nombre'] . ' ' . 'Est paire!';
+}
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job6</title>
+</head>
+<body>
+    <form action ="index.php" method="POST">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre"> </br>
+        <input type="submit" name="submit">
+    
+</body>
+</html>
