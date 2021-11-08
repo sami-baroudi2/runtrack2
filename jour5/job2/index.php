0713de7b60d82fd5876605966282d14d96e13562
sami <sami.baroudi@laplateforme.io>
2021-11-08 10:47:31 +0100

    job 2

--- jour5/job2/index.php
+++ jour5/job2/index.php
@@ -1,1 +1,15 @@
 
+<?php
+
+function bonjour ($jour) {
+    if ($jour == true) {
+        echo 'bonjour';
+        
+    }
+    else  {
+        echo 'bonsoir';
+    }
+}
+bonjour(true)  ;
+bonjour(false) ; 
+?>
