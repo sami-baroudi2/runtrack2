a53d3cd08cda06a7b755aa660399bf675f5d5d30
sami <sami.baroudi@laplateforme.io>
2021-11-08 14:04:43 +0100

    job 5

--- jour5/job5/index.php
+++ jour5/job5/index.php
@@ -1,1 +1,15 @@
+<?php
+$str = "Preuve est faite que visages dévots et pieuses actions nous servent à enrober de sucre le Diable lui-même.";
+$char = "e";
+ function occurence($str , $char) {
+     for  ($i=0; isset($str[$i]) ; $i++) {
+         if ($str[$i] == $char) {
+             $e++;
 
+         }
+     }
+     return $e;
+ }  
+echo occurence ($str , $char);
+
+?>
