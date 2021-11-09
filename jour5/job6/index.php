1e8543ba6563c04d1e961de2bb233d083049c980
sami <sami.baroudi@laplateforme.io>
2021-11-09 10:11:28 +0100

    job 6 final

--- jour5/job6/index.php
+++ jour5/job6/index.php
@@ -1,1 +1,48 @@
+<?php
+$str = "Les roses sont bleues";
+function leetspeak($str){
+    
+    for($i=0;isset($str[$i]);$i++){
 
+        
+
+        if ($str[$i] == 'a' || $str[$i] == 'A') {
+            
+                $str[$i] = '4';
+        }
+        elseif ($str[$i] == 'b' || $str[$i] == 'B') {
+                
+                $str[$i] = '8';
+            }
+        elseif ($str[$i] == 'e' || $str[$i] == 'E') { 
+                
+                $str[$i] = '3';
+            }
+        elseif ($str[$i] == 'g' || $str[$i] == 'G') { 
+                
+                $str[$i] = '6';
+            }
+        elseif ($str[$i] == 'l' || $str[$i] == 'L') { 
+                
+                $str[$i] = '1';
+            }
+        elseif ($str[$i] == 's' || $str[$i] == 'S') { 
+                
+                $str[$i] = '5'; 
+            }
+        elseif ($str[$i] == 't' || $str[$i] == 'T') {
+                
+                $str[$i] = '7';
+            } 
+        else {
+            $str[$i];
+        }
+        
+    }
+    return $str;
+} 
+
+
+    
+echo leetspeak ($str) ;
+?>
